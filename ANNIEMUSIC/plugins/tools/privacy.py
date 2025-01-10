332ab712ec35a3bbbd7bd3cb44c42c6a109631b1
DeadliestOne <uncountableaura@gmail.com>
2025-01-11 03:41:24 +0530

    Update privacy.py

--- ANNIEMUSIC/plugins/tools/privacy.py
+++ ANNIEMUSIC/plugins/tools/privacy.py
@@ -5,7 +5,7 @@ from ANNIEMUSIC import app
 import config
 
 TEXT = f"""
-```🔒ᴘʀɪᴠᴀᴄʏ ᴘᴏʟɪᴄʏ ғᴏʀ {app.username}``` !
+**🔒ᴘʀɪᴠᴀᴄʏ ᴘᴏʟɪᴄʏ ғᴏʀ {app.username}** !
 
 **ʏᴏᴜʀ ᴘʀɪᴠᴀᴄʏ ɪs ɪᴍᴘᴏʀᴛᴀɴᴛ ᴛᴏ ᴜs. ᴛᴏ ʟᴇᴀʀɴ ᴍᴏʀᴇ ᴀʙᴏᴜᴛ ʜᴏᴡ ᴡᴇ ᴄᴏʟʟᴇᴄᴛ, ᴜsᴇ, ᴀɴᴅ ᴘʀᴏᴛᴇᴄᴛ ʏᴏᴜʀ ᴅᴀᴛᴀ, ᴘʟᴇᴀsᴇ ʀᴇᴠɪᴇᴡ ᴏᴜʀ ᴘʀɪᴠᴀᴄʏ ᴘᴏʟɪᴄʏ ʜᴇʀᴇ** : [ᴘʀɪᴠᴀᴄʏ ᴘᴏʟɪᴄʏ](https://telegra.ph/Privacy-Policy-for-01-10).
 
