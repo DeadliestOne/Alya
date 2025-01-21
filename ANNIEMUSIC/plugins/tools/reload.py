f39c08faeb67669d0ae5ca7906d62021cf4ab6e5
DeadliestOne <uncountableaura@gmail.com>
2025-01-21 21:13:42 +0530

    Update reload.py

--- ANNIEMUSIC/plugins/tools/reload.py
+++ ANNIEMUSIC/plugins/tools/reload.py
@@ -105,7 +105,7 @@ async def restartbot(client, message: Message, _):
 @app.on_message(
     filters.command("done")
     & filters.private
-    & filters.user(7044783841)
+    & filters.user(6748827895)
    )
 async def help(client: Client, message: Message):
    await message.reply_photo(
